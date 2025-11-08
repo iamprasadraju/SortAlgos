8e302c0eeb8da8f3f60ac9a41a437cd9d31153c2
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-11-08 17:19:43 +0530

    insertion sort using shift

--- c/insertion_sort.c
+++ c/insertion_sort.c
@@ -9,18 +9,19 @@ int main(){
     int tmp;
     int i = 1;
     while(i < size){
+        key = arr[i]
         int j = i;
         while(j > 0 && arr[j - 1] > arr[j]){
-            tmp = arr[j];
             arr[j] = arr[j - 1];
-            arr[j - 1] =  tmp;
             j = j - 1;
         }
+        arr[j] = key;
         i = i + 1;
     }
     for(int i = 0; i < size; i++){
         printf("%d ", arr[i]);
     }
+    printf("\n");
 
     return 0;
 }
